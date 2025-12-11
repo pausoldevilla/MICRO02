93114616824c2515bf00dd1ad61bc62f3011ff00
Lucía Abellán <luciaabellan6@gmail.com>
2025-12-11 20:33:18 +0100

    Cambios responsive

--- js/nav.js
+++ js/nav.js
@@ -10,26 +10,7 @@ window.onload = () => {
                     d="M480-480q-66 0-113-47t-47-113q0-66 47-113t113-47q66 0 113 47t47 113q0 66-47 113t-113 47ZM160-160v-112q0-34 17.5-62.5T224-378q62-31 126-46.5T480-440q66 0 130 15.5T736-378q29 15 46.5 43.5T800-272v112H160Z" />
             </svg>
             ${userLogged.username}
-            <button class="btn-logout">X</button>
+            <button class="btn-logout" onclick="logout()">X</button>
         `;
     }
-}
-
-// Función para abrir/cerrar el menú
-function toggleMenu() {
-    var menu = document.getElementById('nav');
-
-    // Si el menú tiene la clase 'active', la quitamos (cerrar)
-    // Si no la tiene, la añadimos (abrir)
-    if (menu.classList.contains('active')) {
-        menu.classList.remove('active');
-    } else {
-        menu.classList.add('active');
-    }
-}
-
-// Función para cerrar el menú
-function cerrarMenu() {
-    var menu = document.getElementById('nav');
-    menu.classList.remove('active');
 }
